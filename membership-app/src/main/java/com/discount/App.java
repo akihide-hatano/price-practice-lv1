5584cbae5b376082682e8934c0d1ab0299b8e5f8
akihide-hatano <a.h.19841202@gmail.com>
2025-12-29 08:09:40 +0900

    price-practice-lv1のプロジェクトを作成

---
+++ membership-app/src/main/java/com/discount/App.java
@@ -0,0 +1,14 @@
+package com.discount;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args){
+        int price = 1200;
+
+        System.out.println("=== 割引アプリ (Lv1) ===");
+        System.out.println("プレミアム会員ですか？ yes / no / q(終了)");
+
+        
+    }
+}
