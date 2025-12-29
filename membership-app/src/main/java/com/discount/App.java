1cea72db88d7aea6a1091fb561c597b5a76e6399
akihide-hatano <a.h.19841202@gmail.com>
2025-12-29 09:49:08 +0900

    yes,noを入れた時に終了するように修正

--- membership-app/src/main/java/com/discount/App.java
+++ membership-app/src/main/java/com/discount/App.java
@@ -9,6 +9,29 @@ public class App {
         System.out.println("=== 割引アプリ (Lv1) ===");
         System.out.println("プレミアム会員ですか？ yes / no / q(終了)");
 
-        
+        try( Scanner scanner = new Scanner(System.in)){
+
+            while (true) {
+                
+                System.out.println("入力 > ");
+                String input = scanner.nextLine().trim();
+                
+                if("q".equalsIgnoreCase(input)){
+                    System.out.println("終了します。ありがとうございました");
+                    break;
+                }
+                
+                if("yes".equalsIgnoreCase(input)){
+                    double finalPrice = price * 0.9;
+                    System.out.println("割引が適用されました。" + finalPrice + "円");
+                    break;
+                }else if("no".equalsIgnoreCase(input)){
+                    System.out.println("割引が適用されました。" + price + "円");
+                    break;
+                }else{
+                    System.out.println("yesかnoを入れてください");
+                }
+            }
+        }
     }
 }
